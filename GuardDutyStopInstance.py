afa6f8a92d1f803a3b3f5e1f5ff42c8a54cb5f94
Chris Henson <hensonengineer@gmail.com>
2019-02-18 20:12:04 -0700

    Update GuardDutyStopInstance.py

--- GuardDutyStopInstance.py
+++ GuardDutyStopInstance.py
@@ -1,17 +1,13 @@
 #GuardDutyAutoStopInstances
-#change region in line 17
+#change region in line 13
 #change timeout in Lambda to 15 seconds
-
-import boto3
-import json
-
 
 def lambda_handler(event, context):
     
      print('loading handler')
      result = json.dumps(event)
-     instanceId = event['detail']['resource']['instanceDetails']['instanceId']
-     type = event['detail']['description']
+     instanceId = event['resource']['instanceDetails']['instanceId']
+     type = event['description']
 
 
      ec2 = boto3.client('ec2', region_name = 'us-west-2')
